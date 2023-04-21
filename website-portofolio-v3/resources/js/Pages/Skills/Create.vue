bd9692fe2d2a47240f4025f1f5cefa937194797a
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-04-21 16:37:30 +0700

    (Half Success) Membuat Penyimpanan Skill Baru

--- website-portofolio-v3/resources/js/Pages/Skills/Create.vue
+++ website-portofolio-v3/resources/js/Pages/Skills/Create.vue
@@ -1,6 +1,19 @@
 <script setup>
     import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-    import { Head , Link} from '@inertiajs/vue3';
+    import { Head , Link, useForm} from '@inertiajs/vue3';
+    import InputError from '@/Components/InputError.vue';
+    import InputLabel from '@/Components/InputLabel.vue';
+    import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import TextInput from '@/Components/TextInput.vue';
+
+    const form = useForm({
+    name: '',
+    image: null,
+});
+
+const submit = () => {
+    form.post(route('skills.store'));
+};
 </script>
 <template>
     <Head title="New Skill" />
@@ -11,8 +24,41 @@
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                Form 
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white">
+                <form class="p-4" @submit.prevent="submit">
+            <div>
+                <InputLabel for="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    autofocus
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+            <div>
+                <InputLabel for="image" value="Image" />
+
+                <TextInput
+                    id="image"
+                    type="file"
+                    class="mt-1 block w-full"
+                    @input="form.image = $event.target.files[0]"
+                />
+
+                <InputError class="mt-2" :message="form.errors.image" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">             
+                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Store
+                </PrimaryButton>
+            </div>
+        </form> 
             </div>
         </div>
     </AuthenticatedLayout>
