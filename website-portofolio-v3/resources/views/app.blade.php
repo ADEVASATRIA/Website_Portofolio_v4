16a76104e9c9657f687162dda4021c730cde25e4
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-04-24 21:51:32 +0700

    Berhasil melakukan Tailwind COnfiguration

--- website-portofolio-v3/resources/views/app.blade.php
+++ website-portofolio-v3/resources/views/app.blade.php
@@ -10,6 +10,15 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Play Fair Font  -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;1,400&family=Poppins&display=swap" rel="stylesheet">
+
+        <!-- Work Sans Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;1,400&family=Poppins&family=Work+Sans:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
--- website-portofolio-v3/tailwind.config.js
+++ website-portofolio-v3/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,11 +11,37 @@ module.exports = {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        fontFamily:{
+            primary: "Playfair Display",
+            body: "work Sans"
+        },
+        container:{
+            padding:{
+                DEFAULT: "1rem",
+                lg: "3rem"
             },
         },
+        extend: {
+            colors:{
+                "light-primary": "#FAF1E6",
+                "light-secondary": "#FDFAF6",
+                "light-tail-100": "#A9DED2",
+                "light-tail-500": "#54BAB9",
+                "dark-primary": "#050402",
+                "dark-secondary": "#1C1D24",
+                "dark-navy-100": "#07567D",
+                "dark-navy-500": "#292D42",
+                accent: {
+                    DEFAULT: "#ac6b34",
+                    hover: "#925a2b",
+                },
+                paragraph: "#878e99",
+                blue: colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red,
+            }
+        }
     },
 
     plugins: [require('@tailwindcss/forms')],
