af25a0f6fd42a2cf136996e5c56e76bcb73a6768
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-04-25 23:30:59 +0700

    (Half Success)Membuat Hero Section

---
+++ website-portofolio-v3/resources/js/Components/Frontend/Hero.vue
@@ -0,0 +1,35 @@
+<script setup>
+    
+</script>
+
+<template>
+    <section id="home" class="lg:h-[85vh] flex items-center bg-light-primary dark:bg-dark-primary py-32 lg:py-0 overflow-hidden">
+        <div class="container mx-auto h-full">
+            <div class="flex flex-col md:flex-row items-center h-full pt-8">
+                <div class="flex-1 flex flex-col items-center lg:items-start">
+                    <p class="text-lg text-accent text-md mb-[22px]">Hey , My name is Adeva </p>
+                    <h1 class="text-4xl leading-[44px] md:text-5l md:leading-light lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]">
+                        I'am Student Computer Science  <br /> At binus malang 
+                    </h1>
+                    <p class="pt-4 pd-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:test-left">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime aliquid non libero 
+                        ullam modi rerum sint dolor reiciendis quasi saepe doloribus, 
+                    </p>
+                    <button class="
+                            btn btn-md
+                            bg-accent
+                            hover:bg-light-secondary hover:text-light-tail-500
+                            dark:hover:bg-dark-secondary dark:hover:text-dark-navy-100
+                            md:btn-lg
+                            transition-all"
+                            > Follow For More
+                    </button>
+                </div>
+                <div class="lg:flex flex-1 justify-center items-center h-full mt-8 md:mt-0 ">
+                    <img class="rounded-lg" src="/images/Hero.png" alt="Hero" />
+                </div>
+            </div>
+        </div>
+
+    </section>
+</template>
--- website-portofolio-v3/resources/js/Pages/Welcome.vue
+++ website-portofolio-v3/resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import FrontendLayout from '@/Layouts/Frontend.vue';
+import Hero from "@/Components/Frontend/Hero.vue";
 import { Head, Link } from '@inertiajs/vue3';
 </script>
 
@@ -7,6 +8,7 @@ import { Head, Link } from '@inertiajs/vue3';
     <Head title="Welcome To Potofolio" />
     <FrontendLayout>
         <!-- Hero primary-->
+        <Hero />
         <!-- Promote tail-100-->
         <!-- About Secondary Color -->
         <!-- SKills tail-100 -->
