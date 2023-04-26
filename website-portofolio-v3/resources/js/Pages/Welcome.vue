f0b051883d50a2c9a048ae5deebfb7633f5a8ab3
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-04-26 20:55:49 +0700

    Berhasil membuat Promote Section & About Section

--- website-portofolio-v3/resources/js/Pages/Welcome.vue
+++ website-portofolio-v3/resources/js/Pages/Welcome.vue
@@ -1,6 +1,8 @@
 <script setup>
 import FrontendLayout from '@/Layouts/Frontend.vue';
 import Hero from "@/Components/Frontend/Hero.vue";
+import Promote from '@/Components/Frontend/Promote.vue';
+import About from '@/Components/Frontend/About.vue';
 import { Head, Link } from '@inertiajs/vue3';
 </script>
 
@@ -10,7 +12,9 @@ import { Head, Link } from '@inertiajs/vue3';
         <!-- Hero primary-->
         <Hero />
         <!-- Promote tail-100-->
+        <Promote />
         <!-- About Secondary Color -->
+        <About />
         <!-- SKills tail-100 -->
         <!-- Portofolio primary colors -->
         <!-- Services Secondary Colors-->
