78bd6f59dcab15827e148089ecc85653d9f5fa9e
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-04-27 10:51:31 +0700

    Berhasil membuat Services Section

--- website-portofolio-v3/resources/js/Pages/Welcome.vue
+++ website-portofolio-v3/resources/js/Pages/Welcome.vue
@@ -6,6 +6,7 @@ import About from '@/Components/Frontend/About.vue';
 import Skills from '@/Components/Frontend/Skills.vue';
 import Portfolio from '@/Components/Frontend/Portfolio.vue';
 import Services from '@/Components/Frontend/Services.vue';
+import ContactMe from '@/Components/Frontend/ContactMe.vue';
 import { Head, Link } from '@inertiajs/vue3';
 
 
@@ -32,6 +33,7 @@ defineProps({
         <!-- Services Secondary Colors-->
         <Services />
         <!-- Contact Primary -->
+        <ContactMe />
     </FrontendLayout>
 </template>
 
