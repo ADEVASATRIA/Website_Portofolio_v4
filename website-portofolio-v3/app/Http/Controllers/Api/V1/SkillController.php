2eeb110fea63794fa8a800a355dba9ca82088307
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-06-10 22:57:14 +0700

    [update] berhasil membuat metode show/post/update API

---
+++ website-portofolio-v3/app/Http/Controllers/Api/V1/SkillController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request\StoreSkillRequest;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
+use App\Http\Resources\SkillResource;
+
+class SkillController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Hello World!'
+        ]);
+    }
+    public function show(Skill $skill)
+    {
+        return new SkillResource($skill);
+    }
+
+    public function store(StoreSkillRequest $request)
+    {
+        // $validatedData = $request->validated();
+        Skill::create($request->validated());
+
+        // Process the validated data and store the skill
+
+        return response()->json([
+            'message' => 'Skill created successfully!'
+        ]);
+    }
+
+    public function update(StoreSkillRequest $request, Skill $skill){
+        $skill->update($request->validated());
+
+        return response()->json([
+            'message' => 'Skill updated successfully!'
+        ]);
+    }
+    
+}
--- website-portofolio-v3/routes/api.php
+++ website-portofolio-v3/routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\SkillController;
+// use App\Http\Controllers\SkillController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'V1'], function(){
+    Route::apiResource('skills', SkillController::class);
 });
+
+// Route::group(['prefix'=> 'api'], function(){
+//     Route::apiResource('skills', SkillController::class);
+// });
